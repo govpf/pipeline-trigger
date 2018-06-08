04f49ddf92c911099a262d999d7f4bca8eb32f70
Sven A. Schmidt <sas@finestructure.co>
2018-06-08 17:30:43 +0200

    Remove pipeline token

--- trigger.py
+++ trigger.py
@@ -13,28 +13,26 @@ def trigger():
 
     parser = argparse.ArgumentParser(description='Tool to trigger and monitor a remote GitLab pipeline', add_help=False)
     parser.add_argument(
-        '-a', '--api-token', help='api token to query status')
+        '-a', '--api-token', required=True, help='personal access token')
     parser.add_argument('-d', '--detached', action='store_true', default=False)
     parser.add_argument('-e', '--env')
     parser.add_argument('-h', '--host', default='https://gitlab.com')
     parser.add_argument('--help', action='help', help='show this help message and exit')
-    parser.add_argument('-p', '--pipeline-token', required=True)
     parser.add_argument('-t', '--target-branch')
     parser.add_argument('-u', '--url-path')
     parser.add_argument('-s', '--sleep', type=int)
     parser.add_argument('project_id')
     args = parser.parse_args()
 
-    assert args.pipeline_token
+    assert args.api_token
     assert args.project_id
     assert args.host
 
-    print('Pipeline token:', args.pipeline_token)
     print('Project id:', args.project_id)
 
-    gl = gitlab.Gitlab(args.host, private_token=args.pipeline_token)
-    proj_b = gl.projects.get(4624517)
-    print(f'Project name: {proj_b.name}')
+    gl = gitlab.Gitlab(args.host, private_token=args.api_token)
+    proj = gl.projects.get(args.project_id)
+    print(f'Project name: {proj.name}')
 
 
 if __name__ == "__main__":
