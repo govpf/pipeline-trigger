44aa124d29511df3db38dd93dbe5ba84f316176e
Sven A. Schmidt <sas@finestructure.co>
2018-06-15 12:46:06 +0200

    Working version (with some features missing)

--- test_trigger.py
+++ test_trigger.py
@@ -7,15 +7,15 @@ import trigger
 class Test(unittest.TestCase):
 
     def test_args_1(self):
-        args = trigger.parse_args('-a foo -e foo1=bar2 -e foo2=bar3 proj'.split())
+        args = trigger.parse_args('-a foo -e foo-1=bar2 -e foo2=bar3 proj'.split())
         assert args.api_token == 'foo'
-        assert args.env == ['foo1=bar2', 'foo2=bar3']
+        assert args.env == ['foo-1=bar2', 'foo2=bar3']
         assert args.project_id == 'proj'
 
     def test_args_2(self):
         with pytest.raises(SystemExit):
             trigger.parse_args('-a foo -e foo1=bar2 foo2=bar3 dangling'.split())
 
-    def test_pargs_envs(self):
-        envs = trigger.parse_envs(['foo1=bar2', 'foo2=bar3'])
-        assert envs == [dict(key='foo1', value='bar2'), dict(key='foo2', value='bar3')]
+    def test_pargs_env(self):
+        envs = trigger.parse_env(['foo-1=bar2', 'foo2=bar3'])
+        assert envs == [dict(key='foo-1', value='bar2'), dict(key='foo2', value='bar3')]
--- trigger.py
+++ trigger.py
@@ -2,9 +2,19 @@
 
 import argparse
 import sys
-from typing import Dict, List
+from time import sleep
+from typing import Dict, List, Optional
 
 import gitlab
+import requests
+
+finished_states = [
+    'failed',
+    'manual',
+    'canceled',
+    'success',
+    'skipped'
+]
 
 
 def parse_args(args):
@@ -15,17 +25,18 @@ def parse_args(args):
         '-a', '--api-token', required=True, help='personal access token')
     parser.add_argument('-d', '--detached', action='store_true', default=False)
     parser.add_argument('-e', '--env', action='append')
-    parser.add_argument('-h', '--host', default='https://gitlab.com')
+    parser.add_argument('-h', '--host', default='gitlab.com')
     parser.add_argument(
         '--help', action='help', help='show this help message and exit')
-    parser.add_argument('-t', '--target-branch')
-    parser.add_argument('-u', '--url-path')
-    parser.add_argument('-s', '--sleep', type=int)
+    parser.add_argument('-r', '--ref', help='target ref (branch, tag, sha)')
+    parser.add_argument('-s', '--sleep', type=int, default=5)
+    parser.add_argument('-t', '--target-branch', help='target branch (deprecated: use -r instead)')
+    parser.add_argument('-u', '--url-path', default='/api/v4/projects')
     parser.add_argument('project_id')
     return parser.parse_args(args)
 
 
-def parse_envs(envs: List[str]) -> List[Dict]:
+def parse_env(envs: List[str]) -> List[Dict]:
     res = []
     for e in envs:
         k, v = e.split('=')
@@ -33,18 +44,90 @@ def parse_envs(envs: List[str]) -> List[Dict]:
     return res
 
 
+def create_pipeline(project_url, token, ref, variables=[]) -> Optional[int]:
+    r = requests.post(
+        f'{project_url}/pipeline',
+        headers={
+            'PRIVATE-TOKEN': token
+        },
+        data=dict(
+            ref=ref,
+            # FIXME: variables=variables
+        )
+    )
+    assert r.status_code == 201, f'expected status code 200, was {r.status_code}'
+    return r.json().get('id', None)
+
+
 def trigger():
     args = parse_args(sys.argv[1:])
 
-    assert args.api_token
-    assert args.project_id
-    assert args.host
+    assert args.api_token, 'api token must be set'
+    assert args.project_id, 'project id must be set'
+    assert args.host, 'host must be set'
+    assert args.url_path, 'url path must be set'
+    assert args.ref or args.target_branch, 'must provide either ref (-r) or target_branch (-t)'
+    assert args.sleep > 0, 'sleep parameter must be > 0'
 
-    print('Project id:', args.project_id)
+    ref = args.ref or args.target_branch
+    proj_id = args.project_id
+    token = args.api_token
+    project_url = f"https://{args.host}{args.url_path}/{proj_id}"
+    variables = []
+    if args.env is not None:
+        variables = parse_env(args.env)
 
-    gl = gitlab.Gitlab(args.host, private_token=args.api_token)
-    proj = gl.projects.get(args.project_id)
-    print(f'Project name: {proj.name}')
+    gl = gitlab.Gitlab(f'https://{args.host}', private_token=token)
+    proj = gl.projects.get(proj_id)
+
+    print(f"Triggering pipeline for ref '{ref}' in project {proj.name} (id: {proj_id})")
+
+    pid = None
+
+    retry = False
+    if retry:
+        pass
+    else:
+        # create pipeline
+        # pipeline = proj.pipelines.create(dict(ref=ref, variables=variables))
+        # pid = pipeline.id
+        pid = create_pipeline(project_url, token, ref, variables)
+        print(f'Pipeline created (id: {pid})')
+
+    assert pid is not None, 'must have a valid pipeline id'
+
+    status = None
+    max_retries = 5
+    retries_left = max_retries
+
+    while status not in finished_states:
+        try:
+            status = proj.pipelines.get(pid).status
+            # reset retries_left if the status call succeeded (fail only on consecutive failures)
+            retries_left = max_retries
+        except Exception as e:
+            print(f'Polling for status failed: {e}')
+            retries_left -= 1
+            if retries_left == 0:
+                print(f'Polling failed {max_retries} consecutive times. Please verify the pipeline url:')
+                print(f'   curl -s -X GET -H "PRIVATE-TOKEN: <private token>" {project_url}/pipelines/{pid}')
+                print('check your api token, or check if there are connection issues.')
+                print()
+                sys.exit(2)
+
+        print('.', end='', flush=True)
+        sleep(args.sleep)
+
+    print()
+
+    if status == 'success':
+        ret = 0
+        print('Pipeline succeeded')
+    else:
+        ret = 1
+        print(f'Pipeline failed with status: {status}')
+
+    sys.exit(ret)
 
 
 if __name__ == "__main__":
